37f00adee1dc3b9f4147ae46ed71fecf69d13838
sheler1988 <93482890+sheler1988@users.noreply.github.com>
2024-11-03 18:57:41 +0330

    1

---
+++ SimpleLoginApp/SimpleLoginApp/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+// Simple Login App
+
+var users = new Dictionary<string, string>()
+{
+	["sheler"] = "123",
+	["samira"] = "456p",
+	["bahar"] = "a789"
+};
+
+int invalidAttempts = 0;
+
+while (true)
+{
+	Console.Clear();
+
+    Console.WriteLine($"Remain Login Attempts: {invalidAttempts}");
+
+    Console.Write("Username: ");
+	var username = Console.ReadLine();
+    Console.Write("Password");
+	var password = Console.ReadLine();
+
+
+	if (users.ContainsKey(username) && users[username] == password)
+	{
+        Console.WriteLine("Welcome to simple login app.");
+		Console.ReadKey();
+		break;
+	}
+	else
+	{
+        Console.WriteLine("Invalid username or password!");
+		invalidAttempts--;
+		if (invalidAttempts == 0)
+		{
+            Console.WriteLine("There's no login attempts!");
+			break;
+		}
+		Console.ReadKey();
+		continue;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
